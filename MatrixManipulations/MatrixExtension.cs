998700909701c3d489e85bf1243b8d396e5675c7
GhostPants12 <54960078+GhostPants12@users.noreply.github.com>
2020-01-23 19:45:59 +0300

    feat: add MatrixManipulations and tests

---
+++ MatrixManipulations/MatrixExtension.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace MatrixManipulations
+{
+    /// <summary>
+    /// Matrix manipulation.
+    /// </summary>
+    public static class MatrixExtension
+    {
+        /// <summary>
+        /// Method fills the matrix with natural numbers, starting from 1 in the top-left corner,
+        /// increasing in an inward, clockwise spiral order.
+        /// </summary>
+        /// <param name="size">Matrix order.</param>
+        /// <returns>Filled matrix.</returns>
+        /// <exception cref="ArgumentException">Throw ArgumentException, if matrix order less or equal zero.</exception>
+        /// <example> size = 3
+        ///     1 2 3
+        ///     8 9 4
+        ///     7 6 5.
+        /// </example>
+        /// <example> size = 4
+        ///     1  2  3  4
+        ///     12 13 14 5
+        ///     11 16 15 6
+        ///     10 9  8  7.
+        /// </example>
+        public static int[,] GetMatrix(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentException($"Size of matrix '{size}' cannot be less or equal zero.");
+            }
+
+            int[,] matrix = new int[size, size];
+
+            int row = 0;
+            int col = 0;
+            int dx = 1;
+            int dy = 0;
+            int dirChanges = 0;
+            int visits = size;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[row, col] = i + 1;
+                visits--;
+                if (visits == 0)
+                {
+                    visits = (size * (dirChanges % 2)) + (size * ((dirChanges + 1) % 2)) - (dirChanges / 2) - 1;
+                    int temp = dx;
+                    dx = -dy;
+                    dy = temp;
+                    dirChanges++;
+                }
+
+                col += dx;
+                row += dy;
+            }
+
+            return matrix;
+        }
+    }
+}
